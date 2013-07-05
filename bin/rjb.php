87108dd5e460ec8866bd00300471c1562096c59f
Ioan CHIRIAC <i.chiriac@yahoo.com>
2013-07-05 23:44:25 +0200

    try to start a daemon script

---
+++ bin/rjb.php
@@ -0,0 +1,187 @@
+#!/usr/bin/env php
+<?php
+    $args = array();
+    parse_str(implode('&', array_slice($argv, 1)), $args);
+    // shows the help
+    if ( isset($args['--help']) ) {
+        echo <<<CLI
+Redis Job Queue - by Ioan Chiriac (Under MIT 2013)
+This script handles job queues and launches workers
+Url : https://github.com/ichiriac/redis-job-queue
+
+Sample :
+
+  rjb --config=./rjb.conj
+
+Usage :
+
+  --config  Specify the configuration file that contains server configuration
+  --status  Display the job manager status
+  --start   Starts the job manager (if it's on daemon mode)
+  --stop    Stops the job manager
+CLI;
+        exit(0);
+    }
+    // read the configuration
+    if ( !isset($args['--config']) ) {
+        $args['--config'] = 'rjb.conf';
+    }
+    if ( file_exists($args['--config']) ) {
+        $config = file_get_contents($args['--config']);
+    } else {
+        echo 'ERROR : Unable to read the configuration : ' . $args['--config'] . "\n";
+        exit(1);
+    }
+    $config = json_decode($config, true);
+    if ( empty($config) ) {
+        echo 'ERROR : JSON syntax error in : ' . $args['--config'] . "\n";
+        exit(1);
+    }
+
+    // define some defaults values
+    if ( empty($config['stats']) ) {
+        $config['stats'] = '/var/log/rjb.stats';
+    }
+
+    // retrieve the PID
+    if ( empty($config['pid']) ) {
+        $config['pid'] = '/var/run/rjb.pid';
+    }
+    $pid = null;
+    if ( file_exists($config['pid']) ) {
+        $pid = file_get_contents($config['pid']);
+    }
+
+    /*
+     * Convert seconds to human readable text.
+     *
+     */
+    function secs_to_h($secs)
+    {
+        $units = array(
+            "week"   => 7*24*3600,
+            "day"    =>   24*3600,
+            "hour"   =>      3600,
+            "minute" =>        60,
+            "second" =>         1,
+        );
+        // specifically handle zero
+        if ( $secs == 0 ) return "0 seconds";
+        $s = "";
+        foreach ( $units as $name => $divisor ) {
+                if ( $quot = intval($secs / $divisor) ) {
+                        $s .= "$quot $name";
+                        $s .= (abs($quot) > 1 ? "s" : "") . ", ";
+                        $secs -= $quot * $divisor;
+                }
+        }
+        return substr($s, 0, -2);
+    }
+
+    // the status command
+    if ( isset($args['--status']) ) {
+        if ( empty($pid) ) {
+            echo 'The RJB service is NOT running' . "\n";
+        } else {
+            echo 'The RJB service is running ...' . "\n";
+            // showing statistics
+            if (
+                !empty($config['stats'])
+                && file_exists($config['stats'])
+            ) {
+                $stats = json_decode(file_get_contents(
+                    $config['stats']
+                ), true);
+                if ( !empty($stats) ) {
+                    echo 'Started at ' . date('Y-m-d H:i:s', $stats['start']);
+                    echo ' - ' . secs_to_h( time() - $stats['start']) . "\n\n";
+                    echo 'Memory   : ' . round($stats['memory'] / 1024 / 1024, 2) . 'MB' . "\n";
+                    echo 'Workers  : ' . $stats['counters']['workers'] . "\n";
+                    echo 'Queue    : ' . $stats['counters']['queue'] . "\n";
+                    echo 'Jobs     : '
+                        . round($stats['counters']['done'] / 1000, 1) . 'M done, '
+                        . $stats['counters']['fail'] . ' fails '
+                        . "\n";
+                }
+            }
+        }
+        exit(0);
+    }
+
+    // restart as daemon
+    if ( isset($args['--restart']) ) {
+        $args['--stop'] = true;
+        $args['--start'] = true;
+    }
+    // stops the daemon
+    if ( isset($args['--stop']) ) {
+        if ( !empty($pid) ) {
+            echo 'Stopping ...';
+            var_dump(posix_kill($pid, SIGQUIT));
+            for( $i = 0; $i < 20; $i++) {
+                if ( !file_exists($config['pid']) ) {
+                    break;
+                }
+                echo '.';
+                sleep(1);
+            }
+            echo "\n";
+            if ( file_exists($config['pid']) ) {
+                echo 'ERROR : Unable to stop the process (PID:' . $pid . ')' ."\n";
+                exit(1);
+            }
+        } else {
+            echo 'RJB is not actually running' . "\n";
+        }
+    }
+
+    // run as daemon
+    if ( isset($args['--start']) ) {
+        if ( !empty($pid) ) {
+            echo 'RJB is actually running (PID:' . $pid .')' . "\n";
+            exit(1);
+        }
+
+    }
+
+    // handle the pid
+    if (!empty($pid)) {
+        if ( posix_getpid() != $pid ) {
+            echo 'ERROR : Process is already running (PID:' .$pid . ')' . "\n";
+            exit(1);
+        }
+    } else {
+        file_put_contents($config['pid'], posix_getpid());
+    }
+
+    // script body
+    class rjb {
+        public static $conf;
+        public static $run = true;
+        static function log( $data ) {
+            if (!empty(self::$conf['log']) ) {
+                $f = fopen( self::$conf['log'], 'a+');
+                fputs($f, date('Y-m-d H:i:s') . "\t" . $data . "\n");
+                fclose($f);
+            }
+        }
+        // closing the process
+        static function close() {
+            self::$run = false;
+        }
+        // bootstrap
+        static function init( $conf) {
+            self::$conf = $conf;
+            self::log('Starting RJB');
+            pcntl_signal(SIGQUIT, array('rjb', 'close'));
+            while(self::$run) {
+                usleep(10 * 1000); // wait 10 ms
+            }
+            self::log('Shutdown requested');
+            unlink( self::$conf['pid'] );
+            exit(0);
+        }
+    }
+    // bootstrap the script
+    rjb::init( $config );
+
