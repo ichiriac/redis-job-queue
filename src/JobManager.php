3308dcbec5094f436b0ef5f05c0637e626b7b617
Ioan CHIRIAC <i.chiriac@yahoo.com>
2013-07-07 23:58:34 +0200

    starting the job manager and worker handler

---
+++ src/JobManager.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * This file is distributed under the MIT Open Source
+ * License. See README.MD for details.
+ * @author Ioan CHIRIAC
+ */
+
+// Handles a queue and dispatch if on a list of workers
+class JobManager {
+    // list of current workers
+    public $workers = array();
+    // instance of redis
+    public $parent;
+    // the queue prefix
+    public $prefix;
+    // max number of running childs
+    public $limit;
+    // initialize a job manager
+    public function __construct(RedisJobQueue $parent, $prefix, $limit = 4) {
+        $this->parent = $parent;
+        $this->prefix = $prefix;
+        $this->limit = $limit;
+    }
+    // stops all childs that not work
+    public function clean() {
+        foreach($this->workers as &$w) {
+            if ( !$w->busy ) unset($w);
+        }
+        return empty($this->workers);
+    }
+    // helper : gets the redis instance
+    public function getRedis() {
+        return $this->parent->getRedis();
+    }
+    // helper : log some output data
+    public function log( $data ) {
+        $this->parent->log( '[' . $this->prefix . '] : ' . $data);
+    }
+    // check and select a free worker
+    public function getWorker() {
+        $worker = null;
+        foreach($this->workers as &$w) {
+            if ( !$w->busy ) {
+                if ( !$worker ) {
+                    $worker = $w;
+                } else {
+                    if ( time() - $w->last_job > 60 ) {
+                        // inactive from 1 minute
+                        unset($w);
+                    }
+                }
+            } else {
+                if ( !$w->alive() ) {
+                    // zombie
+                    unset($w);
+                } elseif ( time() - $w->last_job > 600 ) {
+                    // run timeout
+                    unset($w);
+                }
+            }
+        }
+        if ( !$worker ) {
+            if ( $this->limit >= count($this->workers) )  {
+                $worker = new JobWorker( $this );
+                $this->workers[] = $worker;
+            }
+        }
+        return $worker;
+    }
+    // check if a new work is available and dispatch it to a worker
+    public function dispatch() {
+        $worker = $this->getWorker();
+        if ( $worker ) {
+            $job = $this->getJob();
+            if ( $job ) {
+                try {
+                    if ( !$worker->process($job) ) {
+                        throw new \Exception(
+                            'the worker is not starting (see logs)'
+                        );
+                    }
+                } catch( \Exception $ex) {
+                    $this->parent->stats['counters']['errors'] ++;
+                    $this->log(
+                        'Worker error #'.$job.' : ' . $ex->getMessage()
+                    );
+                    $this->requeue( $job );
+                }
+            }
+        }
+    }
+    // puts the job in the queue
+    public function requeue( $job ) {
+        $this->parent['stats']['fail'] ++;
+        $redis = $this->getRedis();
+        if ( $redis ) {
+            $this->log(
+                'CRITICAL ERROR - The job #'
+                . $job
+                . ' was NOT REQUEUED (possible loss of data)'
+            );
+            return null;
+        } else {
+            $redis->lpush( $this->prefix . '.queue', $job );
+        }
+    }
+    // gets a new job
+    public function getJob() {
+        $redis = $this->getRedis();
+        if ( $redis ) return null;
+        return $redis->lpop( $this->prefix . '.queue' );
+    }
+}
---
+++ src/JobWorker.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * This file is distributed under the MIT Open Source
+ * License. See README.MD for details.
+ * @author Ioan CHIRIAC
+ */
+
+// worker class :
+class JobWorker {
+    public $busy = false;
+    public $last_job = 0;
+    public $task;
+    private $parent;
+    private $process;
+    private $std_in;
+    private $std_out;
+    private $std_err;
+    // initialize a worker
+    public function __construct( JobManager $parent ) {
+        $this->parent = $parent;
+        $this->parent['stats']['workers'] ++;
+    }
+    // close the current process
+    public function __destruct() {
+        $this->parent['stats']['workers'] --;
+        $this->kill();
+    }
+    // gets the current process : if not started create it
+    public function getProcess() {
+        if ( !$this->process ) {
+            $descriptorspec = array(
+               0 => array("pipe", "r"),
+               1 => array("pipe", "w"),
+               2 => array("pipe", "a")
+            );
+            $this->process = proc_open(
+                'php -f '
+                . dirname($_SERVER['PHP_SELF'])
+                . '/worker.php',
+                $descriptorspec,
+                $pipes,
+                '/tmp',
+                array(
+                    'config' => $this->parent->parent->conf,
+                    'prefix' => $this->parent->prefix
+                )
+            );
+            if ( $this->process === false ) {
+                $error = error_get_last();
+                $this->parent->log(
+                    'Error : unable to start a process - '
+                    . $error['message']
+                );
+            } else {
+                $this->std_out  = $pipes[0];
+                $this->std_in   = $pipes[1];
+                $this->std_err  = $pipes[2];
+            }
+        }
+        return $this->process;
+    }
+    // gets the current process id
+    public function getPid() {
+        $status = proc_get_status( $this->getProcess() );
+        return isset($status['pid']) ? $status['pid'] : null;
+    }
+    // send some data to process
+    public function send( $data ) {
+        $process = $this->getProcess();
+        if ( $process ) {
+            if ( fwrite($this->std_out, strlen($data) . "\n") === false) {
+                return false;
+            }
+            if ( fwrite($this->std_out, $data . "\n") === false) {
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // process the specified task id
+    public function process($task) {
+        $redis = $this->parent->getRedis();
+        if ( $redis ) return false;
+        $this->task = $task;
+        $key = 'rjq.' .  $this->task;
+        // put the task into a pending state
+        if ( !$redis->hsetnx(
+            $this->parent->prefix . '.pending',
+            $task,
+            time()
+        ) ) {
+            // already pending
+            $this->parent->log(
+                'Error job ' . $this->task . ' is already pending (zombi?)'
+            );
+            return false;
+        }
+        // sends the commands to the process
+        try {
+            if ( !$this->send( $this->task ) ) return false;
+            if ( !$this->send( $redis->hget( $key, 'args' ) ) ) return false;
+        } catch( \Exception $ex ) {
+            $this->parent->log(
+                'Error during at the job ' . $this->task . ' start : '
+                . $ex->getMessage()
+            );
+            return false;
+        }
+        // flag the working state
+        $this->busy = true;
+        $this->last_job = time();
+        try {
+            $redis->hmset(
+                $key,
+                array(
+                    'state' => 'progress',
+                    'time' => $this->last_job,
+                    'srv' => $this->parent->parent->$host,
+                    'pid', $this->getPid(),
+                )
+            );
+        } catch(\Exception $ex) {
+            $this->parent->log(
+                'Warning : could not flag the job ' . $this->task . ' state : '
+                . $ex->getMessage()
+            );
+         }
+        return true;
+    }
+    // kill the current process
+    public function kill() {
+        if ( $this->process ) {
+            if ( $this->busy ) {
+                $this->parent->requeue(
+                    $this->task
+                );
+            }
+            proc_close($this->process);
+            $this->process = null;
+        }
+    }
+    // check if the process is alive or not
+    public function alive() {
+        if ( $this->process ) {
+            $status = proc_get_status( $this->get );
+            return isset($status['running']) ? $status['running'] : false;
+        } else {
+            return false;
+        }
+    }
+}
