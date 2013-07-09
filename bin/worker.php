1ab221eec2ab8422e78af1c4c69fc747c876fcc2
Ioan CHIRIAC <i.chiriac@yahoo.com>
2013-07-09 08:53:49 +0200

    start the worker

---
+++ bin/worker.php
@@ -0,0 +1,10 @@
+#!/usr/bin/env php
+<?php
+/**
+ * This file is distributed under the MIT Open Source
+ * License. See README.MD for details.
+ * @author Ioan CHIRIAC
+ */
+
+while( $cmd = trim(fgets(STDIN)) ) {
+}
