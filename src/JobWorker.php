42856da3c456100123d829391287218e271ed7c3
Ioan CHIRIAC <i.chiriac@yahoo.com>
2013-07-14 12:26:19 +0200

    various fixes + avoid libevent dependency

--- src/JobWorker.php
+++ src/JobWorker.php
@@ -15,14 +15,15 @@ class JobWorker {
     private $std_in;
     private $std_out;
     private $std_err;
+    private $event;
     // initialize a worker
     public function __construct( JobManager $parent ) {
         $this->parent = $parent;
-        $this->parent['stats']['workers'] ++;
+        $this->parent->parent->stats['counters']['workers'] ++;
     }
     // close the current process
     public function __destruct() {
-        $this->parent['stats']['workers'] --;
+        $this->parent->parent->stats['counters']['workers'] --;
         $this->kill();
     }
     // gets the current process : if not started create it
@@ -31,17 +32,20 @@ class JobWorker {
             $descriptorspec = array(
                0 => array("pipe", "r"),
                1 => array("pipe", "w"),
-               2 => array("pipe", "a")
+               2 => array("pipe", "w")
             );
+            $cmd =
+                PHPBIN
+                .' -f '
+                . realpath(__DIR__. '/../bin/')
+                . '/worker.php'
+            ;
             $this->process = proc_open(
-                'php -f '
-                . dirname($_SERVER['PHP_SELF'])
-                . '/worker.php',
+                $cmd,
                 $descriptorspec,
                 $pipes,
                 '/tmp',
                 array(
-                    'config'    => $this->parent->parent->conf,
                     'prefix'    => $this->parent->prefix,
                     'bootstrap' => $this->parent->script
                 )
@@ -56,9 +60,77 @@ class JobWorker {
                 $this->std_out  = $pipes[0];
                 $this->std_in   = $pipes[1];
                 $this->std_err  = $pipes[2];
+                stream_set_blocking($this->std_err, 0);
+                $this->parent->log(
+                    stream_get_contents($this->std_err)
+                );
+                if ( function_exists('event_new') ) {
+                    $this->event = event_new();
+                    event_set(
+                        $this->event,
+                        $this->std_in,
+                        EV_READ | EV_PERSIST,
+                        array($this,'onRead'),
+                        array($this->event, $this->parent->events)
+                    );
+                    event_base_set($this->event, $this->parent->events);
+                    event_add($this->event);
+                }
             }
         }
         return $this->process;
+    }
+    // alternative way to dispatch reading events
+    public function dispatch() {
+        if ( $this->std_in && $this->busy ) {
+            $this->onRead();
+            if ( !$this->alive() ) {
+                $this->parent->log('The worker process is dead');
+                $this->kill();
+                return;
+            }
+        }
+    }
+    // receives a message from the process
+    public function onRead() {
+        $cmd = stream_get_contents($this->std_in);
+        $err = stream_get_contents($this->std_err);
+        if ( !empty($err) ) {
+            $this->parent->log('ERROR : ' . $err);
+        }
+        if ( empty($cmd) ) return;
+        $this->parent->log('child > ' . $cmd);
+        switch( $cmd ) {
+            case 'done':
+                $redis = $this->parent->getRedis(true);
+                $key = 'rjq.' .  $this->task;
+                $redis->hmset(
+                    $key,
+                    array(
+                        'state' => 'done',
+                        'time' => time(),
+                        'duration' => time() - $this->last_job,
+                    )
+                )->read();
+                $this->busy = false;
+                $this->last_job = time();
+                break;
+            case 'error':
+                $redis = $this->parent->getRedis(true);
+                $key = 'rjq.' .  $this->task;
+                $redis->hmset(
+                    $key,
+                    array(
+                        'state' => 'error',
+                        'time' => time()
+                    )
+                )->read();
+                $this->busy = false;
+                $this->last_job = time();
+                break;
+            default:
+                $this->parent->log('bad worker response : ' . $cmd );
+        }
     }
     // gets the current process id
     public function getPid() {
@@ -82,8 +154,7 @@ class JobWorker {
     }
     // process the specified task id
     public function process($task) {
-        $redis = $this->parent->getRedis();
-        if ( $redis ) return false;
+        $redis = $this->parent->getRedis(true);
         $this->task = $task;
         $key = 'rjq.' .  $this->task;
         // put the task into a pending state
@@ -91,7 +162,7 @@ class JobWorker {
             $this->parent->prefix . '.pending',
             $task,
             time()
-        ) ) {
+        )->read() ) {
             // already pending
             $this->parent->log(
                 'Error job ' . $this->task . ' is already pending (zombi?)'
@@ -137,15 +208,20 @@ class JobWorker {
                 $this->parent->requeue(
                     $this->task
                 );
+                $this->busy = false;
             }
             proc_close($this->process);
+            if ( $this->event ) {
+                event_del($this->event);
+                event_free($this->event);
+            }
             $this->process = null;
         }
     }
     // check if the process is alive or not
     public function alive() {
         if ( $this->process ) {
-            $status = proc_get_status( $this->get );
+            $status = proc_get_status( $this->process );
             return isset($status['running']) ? $status['running'] : false;
         } else {
             return false;
