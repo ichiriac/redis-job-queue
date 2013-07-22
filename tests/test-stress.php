7956d167c9308afbe61f61a7ee6132098e546d2f
Ioan CHIRIAC <i.chiriac@yahoo.com>
2013-07-22 23:38:20 +0200

    fix the test output

--- tests/test-stress.php
+++ tests/test-stress.php
@@ -3,10 +3,11 @@ echo '-- stress tests --' . "\n";
 do {
     $load = rand(10, 2000);
     $complexity = rand(1, 20);
-    echo 'Run ' . $load . ' load with ' . $complexity . ' max complexity' . "\n";
+    echo '=== Run ' . $load . ' load with ' . $complexity . ' max complexity' . "\n";
     $start = microtime(true);
     exec('php ' . __DIR__ . '/loader.php ' . $load . ' ' . $complexity);
-    echo '--> duration ' . round(microtime(true) - $start, 1) . 'sec' . "\n";
+    echo '--> duration ' . round(microtime(true) - $start, 1) . 'sec' . "\n\n";
+    $out = array();
     exec('rjq --status', $out);
-    echo implode("\n", $out) . "\n";
+    echo implode("\n", array_slice($out, 4)) . "\n\n";
 } while(true);
